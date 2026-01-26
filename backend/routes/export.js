08a24ffcc3ad8e0bbd74fecf9e8516b310451d64
Chandrika M <162449370+Chandrika9906@users.noreply.github.com>
2026-01-26 22:36:07 +0530

    Delete backend directory

--- backend/routes/export.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../middleware/auth');
-const exportController = require('../controllers/exportController');
-
-router.get('/', auth, exportController.exportData);
-
-module.exports = router;
